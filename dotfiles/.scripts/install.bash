b8c8f50917da28f918ddb5d027b1da5c22af5bb1
Raveen Kumar <raveen.srk.kumar@gmail.com>
2021-11-14 15:28:23 +0530

    u

--- dotfiles/.scripts/install.bash
+++ dotfiles/.scripts/install.bash
@@ -3,50 +3,15 @@
 set -e
 # set -x
 
-read -p "Choose distro: " distro
+read -p "Enter f for fedora or u for ubuntu: [f/u]" distro
 
-case $distro in
-    fedora)
-        sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-        sudo dnf install bat \
+pkg_list_common="
+             bat \
              curl \
              ffmpeg \
              ffmpegthumbnailer \
              gnuplot \
-             htop \
-             ImageMagick \
-             kdialog \
-             kdiff3 \
-             mediainfo \
-             mlocate \
-             mpv \
-             neofetch \
-             newsboat \
-             python \
-             python3 \
-             python3-pip \
-             ranger \
-             ShellCheck \
-             stow \
-             tldr \
-             vim \
-             yank \
-             obs-studio \
-             pandoc
-        ;;
-    ubuntu)
-        sudo apt  update
-        sudo apt  upgrade
-        sudo apt  autoremove
-        sudo apt  autoclean
-        sudo apt  autopurge
-        sudo apt  install bat \
-             curl \
-             ffmpeg \
-             ffmpegthumbnailer \
-             gnuplot \
-             htop \
-             imagemagick \
+             htop \ 
              kdialog \
              kdiff3 \
              mediainfo \
@@ -63,7 +28,33 @@ case $distro in
              tldr \
              vim \
              vim-gtk \
-             yank
+             yank \
+             pandoc \
+             obs-studio
+             "
+pkg_list_ubuntu_only="
+             shellcheck \
+             imagemagick \
+                 "
+pkg_list_fedora_only="
+             ShellCheck \
+             ImageMagick \
+                 "
+
+case $distro in
+    f)
+        sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+        sudo dnf install  "$pkg_list_common"
+        sudo dnf install  "$pkg_list_fedora_only"
+        ;;
+    u)
+        sudo apt  update
+        sudo apt  upgrade
+        sudo apt  autoremove
+        sudo apt  autoclean
+        sudo apt  autopurge
+        sudo apt  install "$pkg_list_common"
+        sudo apt  install "$pkg_list_ubuntu_only"
         ;;
     *)
         echo "Unknown Distro! 😠"
@@ -71,45 +62,22 @@ case $distro in
         ;;
 esac
 
-
-if [[ -d ~/dotfiles-personal ]]; then
-    dotfiles=~/dotfiles-personal
-elif [[ -d ~/.dotfiles-personal ]]; then
-    dotfiles=~/.dotfiles-personal
-else
-    echo "Missing dotfiles repo!"
-    exit 2
-fi
-
-pushd $dotfiles
-stow  dotfiles -t ~/
-popd
-
-echo "source ~/.bash_aliases" >> ~/.bashrc
-
 python3 -m pip install bdfr --upgrade
 
 [ -d ~/tmp ] || mkdir $HOME/tmp
 [ -d ~/.local/bin ] || mkdir -p ~/.local/bin
 
-[ -d ~/.fzf ] && rm -rf ~/.fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+if [ ! -d ~/.fzf ]; then
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+fi
 
-# [ -d ~/tmp/autojump ] && rm -rf ~/tmp/autojump
-# git clone git://github.com/wting/autojump.git
-# ~/tmp/autojump/install.py
+[ ! -d ~/.vim/undo ] && mkdir -p ~/.vim/undo
+[ ! -d ~/.vim/backup ] && mkdir -p ~/.vim/backup
+[ ! -d ~/.vim/swap ] && mkdir -p ~/.vim/undo
 
-# [ -d ~/tmp/ble.sh ] && rm -rf ~/tmp/ble.sh
-# git clone --recursive https://github.com/akinomyoga/ble.sh.git
-# make -C ble.sh install PREFIX=~/.local
-# echo 'source ~/.local/share/blesh/ble.sh' >>~/.bashrc
-
-pushd $HOME/.local/bin
-
-[ -f ~/.local/bin/up ] && rm ~/.local/bin/up
-wget https://github.com/akavel/up/releases/download/v0.3.2/up -O up
-chmod +x up
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 [ -f ~/.local/bin/cht.sh ] && rm ~/.local/bin/cht.sh
 curl https://cht.sh/:cht.sh >$HOME/.local/bin/cht.sh
@@ -117,15 +85,12 @@ chmod +x $HOME/.local/bin/cht.sh
 
 curl -sL "https://raw.githubusercontent.com/pystardust/ytfzf/master/ytfzf" | sudo tee $HOME/.local/bin/ytfzf >/dev/null && sudo chmod 755 $HOME/.local/bin/ytfzf
 
-curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o $HOME/.local/bin/youtube-dl
 sudo chmod a+rx $HOME/.local/bin/youtube-dl
 
-popd
+[ ! -v tldr ] && pip install tldr || echo -e ${YELLOW} tldr install failed ${NC}
 
-# pip install tldr
-# Other useful repos
+pushd ~/tmp
 motivate || git clone https://github.com/mubaris/motivate.git && cd motivate/motivate && sudo ./install.sh && source ~/.bashrc
 
 # git clone https://github.com/mubaris/motivate.git
@@ -135,10 +100,5 @@ motivate || git clone https://github.com/mubaris/motivate.git && cd motivate/mot
 
 # export PATH=$PWD:$PATH
 # echo 'export PATH=$PWD:$PATH' >> ~/.zshrc
-
-# Other
-#--------------
-mkdir -p ~/.local/bin/
-curl https://cht.sh/:cht.sh > ~/.local/bin/cht.sh
-chmod +x ~/.local/bin/cht.sh
+popd
 
