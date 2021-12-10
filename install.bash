783316436848c91b2bdfe641d709eb06fc7cc991
Raveen Kumar <raveen.srk.kumar@gmail.com>
2021-12-10 19:24:39 +0530

    Install script cleanup

--- install.bash
+++ install.bash
@@ -9,8 +9,9 @@ set -e
 # Current script dir csd
 csd=$(dirname "$0")
 
+# {{{ Packages installed with package manager
 
-echo -e "${YELLOW}Enter f/u/n fedora/ubuntu/none:${NC}"
+echo -e "${YELLOW}Enter f/u/n fedora/ubuntu/none (Choose n if you have no root access):${NC}"
 read -r distro
 
 pkg_list_common="bat curl ffmpeg ffmpegthumbnailer gnuplot htop  kdialog kdiff3 mediainfo mlocate mpv neofetch newsboat python python3 python3-pip ranger stow tldr vim yank pandoc obs-studio urlview"
@@ -39,27 +40,36 @@ case $distro in
         ;;
 esac
 
-# python3 -m pip install bdfr --upgrade
+# }}} 
 
+[ -d "$HOME/tmp" ] || mkdir "$HOME/tmp"
+[ -d "$HOME/.local/bin" ] || mkdir -p "$HOME/.local/bin"
+
+# {{{ Stow packages
 pushd "$csd"
 stow dotfiles -R -t "$HOME/"
 popd
+# }}}
 
+# {{{ Sourcing aliases
 set +e
 grep -q "source \"$HOME/.bash_aliases\"" "$HOME/.bashrc"
 if [ $? != 0 ]; then
     echo "source \"$HOME/.bash_aliases\"" >> "$HOME/.bashrc"
 fi
 set -e
+# }}}
 
-[ -d "$HOME/tmp" ] || mkdir "$HOME/tmp"
-[ -d "$HOME/.local/bin" ] || mkdir -p "$HOME/.local/bin"
+# {{{ FZF
 
 if [ ! -d "$HOME/.fzf" ]; then
     git clone --depth 1 "https://github.com/junegunn/fzf.git" "$HOME/.fzf"
     "$HOME/.fzf/install"
 fi
 
+# }}}
+
+# {{{ VIM
 [ ! -d "$HOME/.vim/undo" ] && mkdir -p "$HOME/.vim/undo"
 [ ! -d "$HOME/.vim/backup" ] && mkdir -p "$HOME/.vim/backup"
 [ ! -d "$HOME/.vim/swap" ] && mkdir -p "$HOME/.vim/undo"
@@ -67,10 +77,13 @@ fi
 [ ! -f "$HOME/.vim/autoload/plug.vim" ] && \
 	curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs \
 		 "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+# }}}
 
+# {{{ CHEAT
 [ -f "$HOME/.local/bin/cht.sh" ] && rm "$HOME/.local/bin/cht.sh"
 curl "https://cht.sh/:cht.sh" > "$HOME/.local/bin/cht.sh"
 chmod +x "$HOME/.local/bin/cht.sh"
+# }}}
 
 # curl -sL "https://raw.githubusercontent.com/pystardust/ytfzf/master/ytfzf" | sudo tee $HOME/.local/bin/ytfzf >/dev/null && sudo chmod 755 $HOME/.local/bin/ytfzf
 
@@ -81,7 +94,8 @@ chmod +x "$HOME/.local/bin/cht.sh"
 
 pushd "$HOME/tmp"
 
-command -v ctags || {
+ # {{{ ctags
+ command -v ctags || {
     git clone "https://github.com/universal-ctags/ctags.git"
     pushd ctags || exit 2
     ./autogen.sh
@@ -90,9 +104,10 @@ command -v ctags || {
     make install -j # may require extra privileges depending on where to install
     popd
 }
+# }}} 
 
+# {{{ VIM
 # https://www.vim.org/git.php
-
 command -v vim || {
     git clone "https://github.com/vim/vim.git"
     cd vim
@@ -101,7 +116,10 @@ command -v vim || {
     make install -j
     cd ..
 }
+# }}} 
 
+# {{{ ENTR
+# This program is used to watch for file changes.
 # command -v entr || {
 #     git clone "git@github.com:eradman/entr.git"
 #     cd entr
@@ -110,7 +128,14 @@ command -v vim || {
 #     PREFIX=$HOME/.local make install
 #     cd ..
 # }
+# }}} 
 
 popd
 
+# {{{ Facebook path picker softlink
+fpp_path=$(realpath "$csd/dotfiles/.local/packages/PathPicker-main/fpp")
+pushd "$HOME/.local/bin"
+ln -s "$fpp_path" .
+popd
+# }}}
 
