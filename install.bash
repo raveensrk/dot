90c70b1a8b8ab4e46f25209f0fd32ead3529e478
Raveen Kumar <raveen.srk.kumar@gmail.com>
2022-01-03 20:54:39 +0530

    Sync commit from raveen@UhIN-LT-142

--- install.bash
+++ install.bash
@@ -55,58 +55,38 @@ esac
 
 # }}} 
 
+
 [ -d "$HOME/tmp" ] || mkdir "$HOME/tmp"
 [ -d "$HOME/.local/bin" ] || mkdir -p "$HOME/.local/bin"
 
-# {{{ Stow packages
 pushd "$csd"
 [ -f "$HOME/.inputrc" ] && rm -ivf "$HOME/.inputrc"
 stow stow -R -t "$HOME/"
 popd
 
-# pushd "$HOME/repos"
-# bash "dotfiles-main/stow/.scripts/,stow_find_targets.bash" # FIXME
-# popd
-# }}}
-
-# {{{ INDEPENDENT SOFTLINKS
 pushd "$csd"
 [ -f "$HOME/.local/bin/fpp" ] && rm "$HOME/.local/bin/fpp"
-ln -s "$(realpath "./stow/.local/packages/PathPicker-main/fpp")" "$HOME/.local/bin/fpp"
+ln -s "$(realpath "./stow/.packages/PathPicker-main/fpp")" "$HOME/.local/bin/fpp"
 popd
-# }}} 
 
-# {{{ FZF
 if [ ! -d "$HOME/.fzf" ]; then
-    git clone --depth 1 "https://github.com/junegunn/fzf.git" "$HOME/.fzf"
+    #git clone --depth 1 "https://github.com/junegunn/fzf.git" "$HOME/.fzf"
+    tar xf ./stow/.packages/fzf.tar.gz --directory "$HOME/.fzf"
     "$HOME/.fzf/install"
 fi
-# }}}
 
-# {{{ VIM
 [ ! -d "$HOME/.vim/undo" ] && mkdir -p "$HOME/.vim/undo"
 [ ! -d "$HOME/.vim/backup" ] && mkdir -p "$HOME/.vim/backup"
 [ ! -d "$HOME/.vim/swap" ] && mkdir -p "$HOME/.vim/undo"
 
-[ ! -f "$HOME/.vim/autoload/plug.vim" ] && \
-	curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs \
-		 "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
-# }}}
 
-# # {{{ CHEAT
-# [ -f "$HOME/.local/bin/cht.sh" ] && rm "$HOME/.local/bin/cht.sh"
-# curl "https://cht.sh/:cht.sh" > "$HOME/.local/bin/cht.sh"
-# chmod +x "$HOME/.local/bin/cht.sh"
-# # }}}
+# [ ! -f "$HOME/.vim/autoload/plug.vim" ] && \
+#	curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs \
+#		 "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 
 # curl -sL "https://raw.githubusercontent.com/pystardust/ytfzf/master/ytfzf" | sudo tee $HOME/.local/bin/ytfzf >/dev/null && sudo chmod 755 $HOME/.local/bin/ytfzf
 
 # sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o $HOME/.local/bin/youtube-dl
 # sudo chmod a+rx $HOME/.local/bin/youtube-dl
 
-[ ! -v tldr ] && pip install tldr || echo -e "${YELLOW} tldr install failed ${NC}"
-
-# {{{ Cleanup
-sudo apt autoclean
-sudo apt autoremove
-# }}}
+# [ ! -v tldr ] && pip install tldr || echo -e "${YELLOW} tldr install failed ${NC}"
