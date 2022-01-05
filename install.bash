3c42213f5340242b14e6e258dbf5b397f901c540
Raveen Kumar <raveen.srk.kumar@gmail.com>
2022-01-05 19:41:39 +0530

    Sync commit from raveen@UhIN-LT-142

--- install.bash
+++ install.bash
@@ -16,7 +16,7 @@ while [ "$1" ]; do
             distro="$1"
             ;;
         --rebuild)
-            rebuild="y"
+            rebuild="Y"
             ;;
         *)
             echo -e "${RED}❌ ERROR! WRONG Argument!${NC}"
@@ -27,14 +27,16 @@ while [ "$1" ]; do
 done
 
 echo -e "${YELLOW}Override options [y/n]...${NC}"
-read -e choice
-if [ "$choice" = "y" ]; then
+read -r choice
+if [ "$choice" = "Y" ]; then
     echo -e "${YELLOW}Choose distro [f/u/m/*]...${NC}"
-    read -e choice
+    read -r choice
     distro="$choice"
+    unset choice
     echo -e "${YELLOW}Choose rebuild [y/n]...${NC}"
-    read -e choice
+    read -r choice
     rebuild="$choice"
+    unset choice
 fi 
 
 echo -e "${YELLOW} Installing for distro $distro...${NC}"
@@ -79,10 +81,10 @@ esac
 [ ! -d "$HOME/.vim/undo" ] && mkdir -p "$HOME/.vim/undo"
 [ ! -d "$HOME/.vim/backup" ] && mkdir -p "$HOME/.vim/backup"
 [ ! -d "$HOME/.vim/swap" ] && mkdir -p "$HOME/.vim/swap"
+[ ! -d "$HOME/.packages_extracted" ] && mkdir "$HOME/.packages_extracted"
 
 if [ "$distro" = "u" ] || [ "$distro" = "f" ]; then
     git clone --depth 1 "https://github.com/junegunn/fzf.git" "$csd/stow_vim_plugins/.fzf" || echo -e "${YELLOW}Not cloning. $csd/.fzf already present...${NC}"
-
     pushd "$HOME/.packages"
     ./clone.bash
     popd
@@ -96,16 +98,11 @@ popd
 
 if [ "$distro" = "m" ]; then
 
-    [ ! -d "$HOME/.packages_extracted" ] && mkdir "$HOME/.packages_extracted"
-
     pushd "$HOME/.packages"
-
-    if [ "$rebuild" = "y" ]; then
-
+    if [ "$rebuild" = "Y" ]; then
         for f in *.tar*; do
             tar xf "$f" --directory="$HOME/.packages_extracted"
         done
-
         for f in *.zip; do
             unzip "$f" -d ~/.packages_extracted
         done
@@ -113,70 +110,83 @@ if [ "$distro" = "m" ]; then
     popd
 
     pushd "$HOME/.packages_extracted"
+    if [ "$rebuild" = "Y" ]; then
+        echo -e "${BLUE}Do you want to install libevent? [Y/n]:${NC}"
+        read -r choice
+        if [ "$choice" = "Y" ]; then
+            pushd libevent-*/
+            ./configure --prefix="$HOME/.local" --enable-shared
+            make -j
+            make install -j
+            pushd
+        fi 
+        unset choice
 
-    if [ "$rebuild" = "y" ]; then
+        echo -e "${BLUE}Do you want to install libevent? [Y/n]:${NC}"
+        read -r choice
+        if [ "$choice" = "Y" ]; then
+            # https://github.com/tmux/tmux/wiki/Installing#building-dependencies
+            pushd tmux-2.6
+            PKG_CONFIG_PATH=$HOME/.local/lib/pkgconfig ./configure --prefix="$HOME/.local"
+            make -j
+            make install -j
+            popd
+        fi 
+        unset choice
 
-        pushd libevent-*/
-        ./configure --prefix=$HOME/.local --enable-shared
-        make -j
-        make install -j
-        pushd
+        echo -e "${BLUE}Do you want to install libevent? [Y/n]:${NC}"
+        read -r choice
+        if [ "$choice" = "Y" ]; then
+            pushd vim82
+            ./configure --prefix="$HOME/.local" # defaults to /usr/local
+            make -j
+            make install -j
+            popd
+        fi 
+        unset choice
 
-        # https://github.com/tmux/tmux/wiki/Installing#building-dependencies
-        pushd tmux-2.6
-        PKG_CONFIG_PATH=$HOME/.local/lib/pkgconfig ./configure --prefix=$HOME/.local
-        make -j
-        make install -j
-        popd
+        echo -e "${BLUE}Do you want to install libevent? [Y/n]:${NC}"
+        read -r choice
+        if [ "$choice" = "Y" ]; then
+            pushd ctags
+            ./autogen.sh
+            ./configure --prefix="$HOME/.local" # defaults to /usr/local
+            make -j
+            make install -j # may require extra privileges depending on where to install
+            popd
+        fi 
+        unset choice
 
-        pushd vim82
-        ./configure --prefix="$HOME/.local" # defaults to /usr/local
-        make -j
-        make install -j
-        popd
+        echo -e "${BLUE}INSTALL BASH Completions? [Y/n] [Default n]:${NC}"
+        read -r choice
+        if [ "$choice" = "Y" ]; then
+            pushd ~/.packages_extracted/bash-completion-2.11
+            autoreconf -i  # if not installing from prepared release tarball
+            ./configure --prefix="$HOME/.local"
+            make -j         # GNU make required
+            make check -j     # optional, requires python3 with pytest >= 3.6, pexpect
+            make install -j   # as root
+            popd
+        fi 
+        unset choice
 
-        pushd ctags
-        ./autogen.sh
-        ./configure --prefix="$HOME/.local" # defaults to /usr/local
-        make -j
-        make install -j # may require extra privileges depending on where to install
-        popd
-
-    fi
-
-    echo -e "${BLUE}INSTALL BASH Completions? [Y/n] [Default n]:${NC}"
-    read -e choice
-    if [ "$choice" = "Y" ]; then
-        pushd ~/.packages_extracted/bash-completion-2.11
-        autoreconf -i  # if not installing from prepared release tarball
-        ./configure --prefix="$HOME/.local"
-        make -j         # GNU make required
-        make check -j     # optional, requires python3 with pytest >= 3.6, pexpect
-        make install -j   # as root
-        popd
-    fi 
-    unset choice
-
-    popd
+    # TODO add urlview as a manual install
+    # TODO install stow manual
+    fi # rebuild
+    popd # .packages_extracted
 elif [ "$distro" = "u" ] || [ "$distro" = "f" ]; then
-
     git clone "https://github.com/tmux-plugins/tpm" "$HOME/.tmux/plugins/tpm" || echo -e "${YELLOW}TPM already exists...${NC}"
-
-    curl -sL "https://raw.githubusercontent.com/pystardust/ytfzf/master/ytfzf" | sudo tee $HOME/.local/bin/ytfzf >/dev/null && sudo chmod 755 $HOME/.local/bin/ytfzf
-
-    sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o $HOME/.local/bin/youtube-dl
-    sudo chmod a+rx $HOME/.local/bin/youtube-dl
-
+    curl -sL "https://raw.githubusercontent.com/pystardust/ytfzf/master/ytfzf" | sudo tee "$HOME/.local/bin/ytfzf" >/dev/null && sudo chmod 755 "$HOME/.local/bin/ytfzf"
+    sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o "$HOME/.local/bin/youtube-dl"
+    sudo chmod a+rx "$HOME/.local/bin/youtube-dl"
 fi
+
+ln -sf "$HOME/.packages_extracted/PathPicker-main/fpp" ~/.local/bin
 
 echo -e "${BLUE}Do you want to install fzf? [Y/n]:${NC}"
 read -r choice
 if [ "$choice" = "Y" ]; then
     "$HOME/.fzf/install" || echo -e "${YELLOW}FZF install failed...${NC}"
 fi 
+unset choice
 
-ln -sf "$HOME/.packages_extracted/PathPicker-main/fpp" ~/.local/bin
-
-# TODO add urlview as a manual install
-
-# tar cf stow_vim_plugins.tar.gz stow_vim_plugins
