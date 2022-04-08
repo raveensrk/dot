a294913672b6d180c5223ee3165eaeea5363f77a
Raveen Kumar <raveen.srk.kumar@gmail.com>
2022-04-08 16:17:32 +0530

    Sync commit from raveen@UhIN-LT-142

--- stow/.scripts/,generate_tags.bash
+++ stow/.scripts/,generate_tags.bash
@@ -1,5 +1,25 @@
 #!/bin/bash
 
-pushd "$MY_REPOS" || exit 2
+while [ "$1" ]; do
+    case "$1" in
+        -i)
+            shift
+            dir=$(realpath "$1")
+            ;;
+        --help|-h)
+            help
+            exit 0
+            ;;
+        *)
+            echo -e "${RED}Wrong option!${NC}"
+            exit 2
+            ;;
+    esac
+    shift
+done 
+
+[ "$dir" = "" ] && echo -e "${RED}-i option is mandatory...${NC}" && exit 2
+
+pushd "$dir" || exit 2
 ctags --verbose -R ./*
 popd || exit 2
