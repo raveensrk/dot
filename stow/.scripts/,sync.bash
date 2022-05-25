64809a2686a91d126bc63ba15235739e7170fb35
Raveen Kumar <raveen.srk.kumar@gmail.com>
2022-05-25 19:49:03 +0530

    Sync commit from raveen@UhIN-LT-142

--- stow/.scripts/,sync.bash
+++ stow/.scripts/,sync.bash
@@ -2,6 +2,6 @@
 
 ,sync_all_repos.bash "$MY_REPOS"
 
-if [ "$USER@$HOSTNAME" = "raveen@UhIN-LT-142" ]; then
-    ,sync_all_repos.bash "/mnt/c/Github"
-fi 
+# if [ "$USER@$HOSTNAME" = "raveen@UhIN-LT-142" ]; then
+#     ,sync_all_repos.bash "/mnt/c/Github"
+# fi 
