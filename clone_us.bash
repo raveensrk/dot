90f28206db36922241db7d0db00a89bb251c2d81
raveensrk <40625660+raveensrk@users.noreply.github.com>
2022-05-30 20:48:02 +0530

    Create clone_us.bash

---
+++ clone_us.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+[ ! -d ~/my_repos ] && mkdir ~/my_repos
+pushd ~/my_repos || exit 2
+
+git clone git@github.com:raveensrk/dotfiles-main.git
+git clone git@github.com:raveensrk/my-scripts-main.git 
+
+popd || exit 2
