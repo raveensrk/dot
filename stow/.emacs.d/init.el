c8730a83716464e8e52c8edb394bd2fa4cb47eb9
Raveen Kumar <raveen.srk.kumar@gmail.com>
2022-07-26 19:21:19 +0530

    org mode babel works for TCL too now

--- stow/.emacs.d/init.el
+++ stow/.emacs.d/init.el
@@ -510,3 +510,9 @@ Version 2019-11-04 2021-02-16"
 ;; #TODO (highlight-phrase #TODO dired-broken-symlink)
 
 (add-hook 'sh-mode-hook 'flymake-shellcheck-load)
+
+
+;; set up babel support
+(require 'org)
+(require 'ob-tcl)
+;; add additional languages with (require 'ob-language)
