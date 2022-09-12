7c1e1df894254f35e04394325fd31dd8d7e28752
Raveen Kumar <raveen.srk.kumar@gmail.com>
2022-09-12 12:52:46 +0530

    Added formatter format-all command

--- upgrade_fedora.bash
+++ upgrade_fedora.bash
@@ -3,3 +3,5 @@
 
 sudo dnf install "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 sudo dnf install  $(xargs < ./packages_list_fedora.txt)
+
+npm install --global prettier @prettier/plugin-lua @prettier/plugin-php prettier-plugin-solidity prettier-plugin-svelte prettier-plugin-toml
