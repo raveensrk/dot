dff05c6747ec00e369ba68dd3ceaafa0c4e921d1
Raveen Kumar <raveen.srk.kumar@gmail.com>
2022-11-24 23:59:36 +0530

    Use vim fugitive for commit instead of emacs magit

--- stow/.scripts/,dotfiles_sync.bash
+++ stow/.scripts/,dotfiles_sync.bash
@@ -52,7 +52,8 @@ else
     # #TODO https://trycatchchris.co.uk/post/view/Open-Emacs-magit-from-command-line
     # emacsclient -nw -c --eval '"'"'(progn (let ((display-buffer-alist `(("^\\*magit: " display-buffer-same-window) ,dis bplay-buffer-alist))) (magit-status)) (delete-other-windows))'"'"'
     
-    emacs --eval "(progn (magit) (delete-other-windows))"
+    # emacs --eval "(progn (magit) (delete-other-windows))"
+    vim -c "Git"
 
 fi
 
