5a35f53b1dc04a8a0f06d9193b13ccf5de947432
Raveen Kumar <raveen.srk.kumar@gmail.com>
2023-01-18 06:20:51 -0600

    Added custom set variables and some more shortcuts

--- stow_my_emacs/.emacs.d/init.el
+++ stow_my_emacs/.emacs.d/init.el
@@ -928,3 +928,17 @@
 
 (add-hook 'context-menu-functions #'man-context-menu)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(projectile-generic-command "fd -L . -0 --type f --color=never --strip-cwd-prefix"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(global-set-key (kbd "ESC ESC b o") 'ivy-switch-buffer-other-window)
