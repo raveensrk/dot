0a34ad7f1cdd1e0b566c8c0d2d277a8a1c815eca
Raveek Kumar <raveen.srk.kumar@gmail.com>
2023-02-21 15:43:34 +0530

    Major emacs init update with imenu and zones

--- stow_my_emacs/.emacs.d/init.el
+++ stow_my_emacs/.emacs.d/init.el
@@ -1,5 +1,8 @@
 ;; Emacs versions supported: 28.2+
 
+;;; IDEAS
+;; https://github.com/abo-abo/auto-yasnippet
+
 ;;; Straight
 (defvar bootstrap-version)
 (let ((bootstrap-file
@@ -29,13 +32,16 @@
 ;; (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 ;; (advice-add 'list-packages :before 'package-refresh-contents)
 ;; (setq use-package-always-ensure t) ; This only works with package.el ;; https://github.com/radian-software/straight.el#integration-with-use-package:~:text=Specifying%20%3Astraight%20t%20is%20unnecessary%20if%20you%20set%20straight%2Duse%2Dpackage%2Dby%2Ddefault%20to%20a%20non%2Dnil%20value.%20(Note%20that%20the%20variable%20use%2Dpackage%2Dalways%2Densure%20is%20associated%20with%20package.el%2C%20and%20you%20should%20not%20use%20it%20with%20straight.el.)
+
 ;;; Use package
 (defalias 'up 'use-package)
-
-(up benchmark-init :straight t :straight t :config (add-hook 'after-init-hook 'benchmark-init/deactivate))
+(use-package diminish
+  :straight t)
+(use-package benchmark-init
+  :straight t
+  :config (add-hook 'after-init-hook 'benchmark-init/deactivate))
 ;;; Startup, behaviour, basics
 (setq tab-width 4)
-
 (setq mouse-wheel-flip-direction t)
 (setq mouse-wheel-tilt-scroll t)
 (setq save-place-mode t)
@@ -46,14 +52,13 @@
 (setq vc-follow-symlinks t)
 (setq ring-bell-function 'ignore)
 (setq visible-bell 1)
-
-;; (tab-bar-mode nil)
 (savehist-mode 1)
 (mouse-avoidance-mode 'cat-and-mouse)
 ;; This will set first buffer shown as scratch
 ;; (setq initial-buffer-choice t)
 ;; (toggle-frame-maximized)
 (put 'narrow-to-region 'disabled nil)
+
 (use-package dashboard
   :straight t
   :diminish
@@ -71,6 +76,13 @@
                           (bookmarks . 10)
                           (projects . 5))))
 ;;;; Editing
+(use-package wrap-region
+  :straight t
+  :diminish
+  :config
+  (wrap-region-global-mode)
+  (wrap-region-add-wrapper "*" "*")
+  )
 (add-hook 'prog-mode-hook (lambda () (define-key prog-mode-map (kbd "ESC ESC c") 'comment-line)))
 
 (defun my-increment-number-decimal (&optional arg)
@@ -95,11 +107,11 @@
 (setq-default indent-tabs-mode nil)
 ;; emacs 23.1 to 26, default to t
 ;; if indent-tabs-mode is t, it means it may use tab, resulting mixed space and tab
-(up smartparens
+(use-package smartparens
   :straight t
   :diminish
   :config (smartparens-global-mode +1))
-(up drag-stuff
+(use-package drag-stuff
   :disabled t
   :straight t
   :defer 5
@@ -129,14 +141,24 @@
 (unless (display-graphic-p)
   (xterm-mouse-mode 1))
 ;;; Appearence
-(setq cursor-type 'bar)
-
+(diminish 'eldoc-mode)
+(use-package dimmer
+  :straight t
+  :config
+  (dimmer-configure-which-key)
+  (dimmer-configure-hydra)
+  (dimmer-configure-magit)
+  (dimmer-configure-posframe)
+  (dimmer-mode t)
+  )
 (setq-default truncate-lines +1)
-(up git-gutter
+(use-package git-gutter
+  :diminish
   :straight t
   :config
   (global-git-gutter-mode)
   (setq git-gutter:always-show-separator t)
+  (diminish 'global-git-gutter-mode)
   )
 (setq display-line-numbers 'visual)
 (setq display-line-numbers-type 'visual)
@@ -157,14 +179,14 @@
 ;; http://xahlee.info/emacs/emacs/elisp_menu.html#:~:text=Adding%20Your%20Own%20Menu&text=(emacs%20call%20these%20IDs%20%E2%80%9Cfake,%3E%20.
 
 ;; Creating a new menu pane in the menu bar to the right of “Tools” menu
-(up nyan-mode
+(use-package nyan-mode
   :straight t
   ;; Nyan mode
   ;; https://github.com/TeMPOraL/nyan-mode
   :config
   (nyan-mode +1)
   )
-(up zoom
+(use-package zoom
   :straight t
   :diminish
   ;; https://github.com/cyrus-and/zoom
@@ -174,8 +196,9 @@
   :straight t
   :if (display-graphic-p))
 ;; TODO make sure you do this on windows https://www.reddit.com/r/emacs/comments/gznezn/alltheicons/
-(up focus
+(use-package focus
   :straight t
+  :disabled t
   :diminish
   :hook emacs-lisp-mode
   )
@@ -192,7 +215,7 @@
 
 ;; Load the theme of your choice
 (load-theme 'modus-vivendi :no-confim)
-(up indent-guide
+(use-package indent-guide
   :straight t
   :diminish
   :config (indent-guide-global-mode 1))
@@ -210,6 +233,39 @@
   (set-frame-font "Inconsolata-12")))
 
 ;; (use-package dynamic-fonts :init (dynamic-fonts-setup))     ; finds "best" fonts and sets faces: default, fixed-pitch, variable-pitch
+
+
+;; Check if a font exists
+(defun font-exists-p (font)
+  "Check if FONT exists on the system."
+  (member font (x-list-fonts "*")))
+
+;; Set the default font to Fira Code with size 14
+(if (font-exists-p "Fira Code")
+    (progn
+      (set-face-attribute 'default nil :font "Fira Code-14")
+
+      ;; Define the prettified symbol list
+      (defvar my-fira-code-prettify-symbols-alist
+        '(("lambda" . ?λ)
+          ("->" . ?→)
+          ("=>" . ?⇒)
+          ("!=" . ?≠)
+          (">=" . ?≥)
+          ("<=" . ?≤)
+          ))
+
+      ;; Enable Fira Code ligatures in programming and text modes
+      (add-hook 'prog-mode-hook
+                (lambda ()
+                  (setq prettify-symbols-alist my-fira-code-prettify-symbols-alist)
+                  (prettify-symbols-mode)))
+      (add-hook 'text-mode-hook
+                (lambda ()
+                  (setq prettify-symbols-alist my-fira-code-prettify-symbols-alist)
+                  (prettify-symbols-mode)))
+      ))
+
 (use-package magit
   :straight t
   :bind ("C-x g" . magit-status))
@@ -251,6 +307,8 @@
   (interactive)
   (indent-region (point-min) (point-max))
   (save-buffer)  )
+(define-key global-map (kbd "C-c =") 'my-indent-whole-buffer)
+
 ;;; Company and completions
 (global-set-key (kbd "C-c C-l") 'my-expand-lines)
 (use-package company
@@ -269,14 +327,23 @@
   :bind
   ("C-M-<mouse-1>" . mc/add-cursor-on-click))
 
-(use-package which-key :straight t :config (which-key-mode 1))
-(use-package rainbow-delimiters :straight t :config (rainbow-delimiters-mode 1))
+(use-package which-key
+  :straight t
+  :config (which-key-mode 1))
+(use-package rainbow-delimiters
+  :straight t
+  :config
+  (rainbow-delimiters-mode 1)
+  :hook emacs-lisp-mode
+  )
 (use-package avy
   :straight t
   :bind ("C-c a" . avy-goto-char))
 ;;; Minibuffer completions
-(use-package marginalia :straight t :config (marginalia-mode 1))
-(up ivy
+(use-package marginalia
+  :straight t
+  :config (marginalia-mode 1))
+(use-package ivy
   :straight t
   :diminish
   :config
@@ -286,12 +353,21 @@
   (setq ivy-count-format "(%d/%d) ")
   ;; enable this if you want `swiper' to use it
   ;; (setq search-default-mode #'char-fold-to-regexp)
+  (setq ivy-re-builders-alist '((t . orderless-ivy-re-builder)))
+  (add-to-list 'ivy-highlight-functions-alist '(orderless-ivy-re-builder . orderless-ivy-highlight))
   :bind
   ("C-x b"   . 'ivy-switch-buffer)
   ("C-c v"   . 'ivy-push-view)
   ("C-c V"   . 'ivy-pop-view))
 
-(up swiper
+(use-package orderless
+  :straight t
+  :after ivy
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles basic partial-completion)))))
+
+(use-package swiper
   :straight t
   :config
   (defun my-word-at-point ()
@@ -302,14 +378,20 @@
   ("C-c *" . my-word-at-point)
   )
 
-(up counsel
+(use-package counsel
   :straight t
+  :config
+  (defun my-counsel-M-x ()
+    "Counsel M-x with ^ removed"
+    (interactive)
+    (counsel-M-x "")
+    )
   :bind (
-	     ("C-c i L" . counsel-git-log)
+	     ("C-c g l" . counsel-git-log)
 	     ("C-c b"   . counsel-bookmark)
 	     ( "C-c c"   . counsel-compile)
 	     ("C-c d"   . counsel-descbinds)
-	     ("C-c g"   . counsel-git)
+	     ("C-c g g"   . counsel-git)
 	     ("C-c j"   . counsel-git-grep)
 	     ("C-c k"   . counsel-rg)
 	     ("C-c m"   . counsel-linux-app)
@@ -319,8 +401,8 @@
 	     ("C-c z"   . counsel-fzf)
 	     ("C-x C-f" . counsel-find-file)
 	     ("C-x l"   . counsel-locate)
-	     ("M-x"     . counsel-M-x)
-	     ("C-c fr"  . counsel-recentf)
+	     ("M-x"     . my-counsel-M-x)
+	     ("C-c f r"  . counsel-recentf)
 	     :map minibuffer-local-map
 	     ("C-r" . counsel-minibuffer-history)
 	     ))
@@ -355,7 +437,7 @@
 
 
 ;;; Projectile
-(up projectile
+(use-package projectile
   :straight t
   :diminish
   :config
@@ -375,7 +457,7 @@
 (setq projectile-auto-discover nil)
 ;;  ;;; Evil
 ;;  (setq evil-want-keybinding nil)
-;;  (up evil-numbers
+;;  (use-package evil-numbers
 ;;    :after evil
 ;;    :straight t
 ;;    :config
@@ -383,13 +465,13 @@
 ;;    (evil-define-key '(normal visual) 'global (kbd "C-x") 'evil-numbers/dec-at-pt)
 ;;    (evil-define-key '(normal visual) 'global (kbd "g C-a") 'evil-numbers/inc-at-pt-incremental)
 ;;    (evil-define-key '(normal visual) 'global (kbd "g C-x") 'evil-numbers/dec-at-pt-incremental))
-;;  (up evil-snipe
+;;  (use-package evil-snipe
 ;;    :after evil
 ;;    :straight t
 ;;    :config
 ;;    (evil-snipe-mode 1)
 ;;    (evil-snipe-override-mode 1))
-;;  (up evil-mc :straight t :config (evil-mc-mode 1))
+;;  (use-package evil-mc :straight t :config (evil-mc-mode 1))
 ;;  (add-hook 'xref--xref-buffer-mode-hook 'turn-off-evil-mode)
 ;;  (add-hook 'Custom-mode-hook 'turn-off-evil-mode)
 
@@ -419,7 +501,7 @@
 ;;    (setq evil-want-integration t)
 ;;    (evil-collection-init))
 ;; ;;;; Evil leader
-;; (up evil-leader
+;; (use-package evil-leader
 ;;   :straight t
 ;;   :after evil
 ;;   :config
@@ -473,11 +555,11 @@
 ;;
 ;; (my-load-elisp-files my-lisp-files)
 ;;; Yas snippets
-(up yasnippet
+(use-package yasnippet
   :straight t
   :diminish
   :bind
-  ("C-c y" . 'yas-new-snippet)
+  ("C-c y n" . 'yas-new-snippet)
   :config
   (yas-global-mode 1) ;; or M-x yas-reload-all if you've started YASnippet already.
   (setq yas-snippet-dirs-windows "c:/github/dotfiles-main/stow_my_emacs/.emacs.d/snippets")
@@ -485,10 +567,13 @@
       (add-to-list 'yas-snippet-dirs yas-snippet-dirs-windows)
     (setq yas-snippet-dirs        '("~/.emacs.d/snippets/")))
   )
+(use-package ivy-yasnippet
+  :straight t
+  :after yasnippet
+  :bind
+  ("C-c y i" . ivy-yasnippet))
 
-
-
-(up yasnippet-snippets
+(use-package yasnippet-snippets
   :after yasnippet
   :straight t
   :diminish)
@@ -513,10 +598,42 @@
         fzf/position-bottom t
         fzf/window-height 15))
 ;;; Convenience
-(up restart-emacs
+(defun copy-file-path-to-clipboard ()
+  "Copy the current file path to the kill ring and clipboard."
+  (interactive)
+  (let ((file-path (buffer-file-name)))
+    (when file-path
+      (kill-new file-path)
+      (message "Copied file path: %s" file-path)
+      (when (region-active-p)
+        (deactivate-mark))
+      (x-select-text file-path))))
+
+
+
+;; File-info
+(use-package hydra
+  :straight t)
+(use-package browse-at-remote
+  :straight t)
+(use-package posframe
+  :straight t)
+(use-package file-info
+  :straight (:host github :repo "artawower/file-info.el")
+  :bind (("C-c f i" . 'file-info-show))
+  :config
+  (setq hydra-hint-display-type 'posframe)
+  (setq hydra-posframe-show-params `(:poshandler posframe-poshandler-frame-center
+                                                 :internal-border-width 2
+                                                 :internal-border-color "#61AFEF"
+                                                 :left-fringe 16
+                                                 :right-fringe 16)))
+
+
+(use-package restart-emacs
   :straight t
   :bind ("C-c r" . restart-emacs))
-(up format-all
+(use-package format-all
   :straight t
   :diminish
   ;; https://github.com/lassik/emacs-format-all-the-code/tree/c156ffe5f3c979ab89fd941658e840801078d091
@@ -562,7 +679,7 @@
     (insert-file-contents file-path)
     (mapcar (lambda (line) (format "%s" line)) (split-string (buffer-string) "\n" t))))
 
-(up elfeed
+(use-package elfeed
   :straight t
   :defer 10
   :if (file-exists-p "~/.emacs.d/my-packages/rss-feeds.el")
@@ -591,12 +708,12 @@
 
 (straight-use-package
  '(aide :type git :host github :repo "junjizhi/aide.el"))
-(up request :straight t)
-(up aide
+(use-package request :straight t)
+(use-package aide
   :config
   (setq aide-max-tokens 200))
 
-(up persistent-scratch
+(use-package persistent-scratch
   :straight t
   :config
   (persistent-scratch-setup-default))
@@ -615,12 +732,12 @@
 ;; https://emacsredux.com/blog/2016/01/31/use-tab-to-indent-or-complete/
 ;; (setq tab-always-indent 'complete)
 
-(up crux
+(use-package crux
   :straight t
   :config
   (global-set-key (kbd "C-c D") 'crux-smart-kill-line)
   (global-set-key (kbd "C-c d") 'crux-duplicate-current-line-or-region))
-(up web-mode
+(use-package web-mode
   :straight t)
 (global-set-key (kbd "C-c i") 'my-open-init-file)
 (global-set-key (kbd "C-c f f") 'ffap)
@@ -631,7 +748,7 @@
   (delete-other-windows))
 (global-set-key (kbd "C-c K") 'nuke-all-buffers)
 
-(up magit-section
+(use-package magit-section
   :straight t)
 
 (global-set-key (kbd "C-c m") 'menu-bar-open)
@@ -659,7 +776,7 @@
 (setq comint-buffer-maximum-size 10000)
 (global-display-line-numbers-mode +1)
 
-(up markdown-mode
+(use-package markdown-mode
   :straight t)
 
 
@@ -674,23 +791,27 @@
 
 
 ;;; Outline mode extend headings backline
-(up outline-minor-faces
-  :straight t)
+(use-package outline-minor-faces
+  :straight t
+  :diminish
+  )
 (use-package backline
+  :diminish
   :straight t
   :after outline
   :config (advice-add 'outline-flag-region :after 'backline-update)
   (outline-minor-faces-mode +1))
 (add-hook 'emacs-lisp-mode-hook 'outline-minor-mode)
 
-;; (up magit-section
+;; (use-package magit-section
 ;;   :straight t)
 
-(up smex
+(use-package smex
   :straight t)
 
 (use-package back-button
   :straight t
+  :diminish
   :config
   (back-button-mode +1))
 
@@ -774,7 +895,7 @@
 (put 'dired-find-alternate-file 'disabled nil)
 (setq-default dired-listing-switches "-alh")
 (setq dired-recursive-copies 'always)
-(up dired-narrow
+(use-package dired-narrow
   :straight t
   :config
   (define-key dired-mode-map (kbd "/") 'dired-narrow-fuzzy)
@@ -833,7 +954,7 @@ Saves to a temp file and puts the filename in the kill ring."
     (message filename)))
 
 
-(up browse-kill-ring
+(use-package browse-kill-ring
   :straight t
   :config
   (setq browse-kill-ring-highlight-inserted-item t
@@ -874,11 +995,6 @@ Saves to a temp file and puts the filename in the kill ring."
           (:help-echo "Local changes not in upstream")))
         ("Path"    99 magit-repolist-column-path ())))
 
-(setq org-export-backends '(ascii html icalendar latex md odt org))
-
-
-
-
 (defun my-magit-repolist-pull-repos (repos)
   "This will get all repos name in repolist"
   (interactive (list (magit-repolist--get-repos ?*)))
@@ -909,7 +1025,7 @@ Saves to a temp file and puts the filename in the kill ring."
 
 
 ;;; Better emacs narrowing, narrow ring 🛣️
-(up zones
+(use-package zones
   :straight t
   :disabled t
   )
@@ -918,22 +1034,12 @@ Saves to a temp file and puts the filename in the kill ring."
 
 (straight-use-package
  '(multifile :type git :host github :repo "magnars/multifiles.el"))
-(up multifiles
+(use-package multifiles
   :load-path "~/.emacs.d/straight/build/multifile")
 (global-set-key (kbd "C-c 3") 'mf/mirror-region-in-multifile)
 
 
-;;; TODO Emojis not working
-
-;;; Enable tabs in buffers
-(global-tab-line-mode -1)
-
-
 (setq org-archive-location "%s::* Archived Tasks")
-
-
-
-
 (setq vc-follow-symlinks nil)
 (setq auto-save-visited-mode t
       calendar-date-style 'iso
@@ -968,8 +1074,6 @@ Saves to a temp file and puts the filename in the kill ring."
       winner-mode t)
 
 (setq-default cursor-type '(bar . 2))
-
-
 
 (define-minor-mode sticky-buffer-mode
   "Make the current window always display this buffer."
@@ -1007,88 +1111,54 @@ Saves to a temp file and puts the filename in the kill ring."
 (use-package openai
   :straight (openai :type git :host github :repo "emacs-openai/openai"))
 
+;;; Eshell
+(use-package eshell
+  :config
+  (defun eshell-new-buffer (args)
+    "Create a new eshell buffer."
+    (interactive "P")
+    (eshell "new")
+    )
+  (global-set-key (kbd "C-c e e") 'eshell)
+  (global-set-key (kbd "C-c e n") 'eshell-new-buffer))
 
-(defun eshell-new-buffer (args)
-  "Create a new eshell buffer."
-  (interactive "P")
-  (eshell "new")
+
+;;; Imenu
+
+(use-package imenu
+  :config
+  (defun try-to-add-imenu ()
+    "Function to enable imenu in any mode, if that mode has imenu available."
+    (condition-case nil (imenu-add-to-menubar "imenu-index") (error nil)))
+  (add-hook 'font-lock-mode-hook 'try-to-add-imenu)
+
+  ;; Add use-package to imenu list
+  (add-to-list 'imenu-generic-expression
+               '("Used Packages"
+                 "\\(^\\s-*(use-package +\\)\\(\\_<.+\\_>\\)" 2))
+  ;; Sort Imenu by name
+  (setq imenu-sort-function 'imenu--sort-by-name)
+  (setq imenu-auto-rescan t)
+  :bind
+  ("C-c o i" . counsel-imenu)
   )
 
-
-(global-set-key (kbd "C-c e e") 'eshell)
-
-(global-set-key (kbd "C-c e n") 'eshell-new-buffer)
-
-(define-key global-map (kbd "C-c =") 'my-indent-whole-buffer)
-
-
-;; Check if a font exists
-(defun font-exists-p (font)
-  "Check if FONT exists on the system."
-  (member font (x-list-fonts "*")))
-
-;; Set the default font to Fira Code with size 14
-(if (font-exists-p "Fira Code")
-    (progn
-      (set-face-attribute 'default nil :font "Fira Code-14")
-
-      ;; Define the prettified symbol list
-      (defvar my-fira-code-prettify-symbols-alist
-        '(("lambda" . ?λ)
-          ("->" . ?→)
-          ("=>" . ?⇒)
-          ("!=" . ?≠)
-          (">=" . ?≥)
-          ("<=" . ?≤)
-          ))
-
-      ;; Enable Fira Code ligatures in programming and text modes
-      (add-hook 'prog-mode-hook
-                (lambda ()
-                  (setq prettify-symbols-alist my-fira-code-prettify-symbols-alist)
-                  (prettify-symbols-mode)))
-      (add-hook 'text-mode-hook
-                (lambda ()
-                  (setq prettify-symbols-alist my-fira-code-prettify-symbols-alist)
-                  (prettify-symbols-mode)))
-      ))
-
-
-
-(defun copy-file-path-to-clipboard ()
-  "Copy the current file path to the kill ring and clipboard."
-  (interactive)
-  (let ((file-path (buffer-file-name)))
-    (when file-path
-      (kill-new file-path)
-      (message "Copied file path: %s" file-path)
-      (when (region-active-p)
-        (deactivate-mark))
-      (x-select-text file-path))))
-
-
-
-;; File-info
-(use-package hydra
+(use-package imenu-anywhere
   :straight t)
-(use-package browse-at-remote
+
+;;; Fun
+
+(use-package zone-sl
   :straight t)
-(use-package posframe
+(use-package zone-select
   :straight t)
-(use-package file-info
-  :straight (:host github :repo "artawower/file-info.el")
-  :bind (("C-c f i" . 'file-info-show))
+(use-package zone
   :config
-  (setq hydra-hint-display-type 'posframe)
-  (setq hydra-posframe-show-params `(:poshandler posframe-poshandler-frame-center
-                                               :internal-border-width 2
-                                               :internal-border-color "#61AFEF"
-                                               :left-fringe 16
-                                               :right-fringe 16)))
-
-(use-package wrap-region
+  (zone-when-idle 10)
+  (zone-select-add-program 'zone-pgm-sl)
+  )
+(use-package zone-rainbow
   :straight t
+  :after zone
   :config
-  (wrap-region-global-mode)
-  (wrap-region-add-wrapper "*" "*")
-  )
+  (setq zone-programs (vconcat [zone-rainbow] zone-programs)))
