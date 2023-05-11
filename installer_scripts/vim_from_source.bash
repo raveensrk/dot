713f2605e91e46b3fa66fec2060379b1558a8c3c
Raveen Kumar <raveen.srk.kumar@gmail.com>
2023-05-11 17:48:58 +0530

    Fix install vim from source script

--- installer_scripts/vim_from_source.bash
+++ installer_scripts/vim_from_source.bash
@@ -4,22 +4,44 @@ echo "Are you in default bash shell with --norc and --noprofile? Press any key t
 read 
 
 pushd ~/.tmp || exit 2
+
 # wget -nc https://ftp.nluug.nl/pub/vim/unix/vim-8.2.tar.bz2
 # tar xf vim-8.2.tar.bz2
 # pushd vim82 || exit 2
-
-# Get vim 9 from git head
-git clone git@github.com:vim/vim.git
-pushd vim || exit 2
+# Get vim 9 from git
+# git clone git@github.com:vim/vim.git
+wget -nc https://github.com/vim/vim/archive/refs/tags/v9.0.1539.tar.gz
+tar xf v9.0.1539.tar.gz
+pushd vim-9.0.1539 || exit 2
 echo -e "${BLUE}Do you want to Install with x? [Y/n]:${NC}"
 read -r choice
+make clean
 if [ "$choice" = "Y" ]; then 
-    # ./configure --prefix="$HOME/.local" --with-features=huge --with-x  --enable-gui=auto --enable-xim --enable-fontset  --enable-python3interp --enable-multibyte --enable-gtk3-check
-    ./configure --prefix="$HOME/.local" --with-features=huge --with-x  --enable-gui=gtk3 --enable-python3interp --enable-gtk3-check
+    if [[ $(uname -a | awk '{print $1}') = "Darwin" ]]; then
+        ./configure \
+            --prefix=$HOME/.local \
+            --with-features=huge \
+            --enable-gui=yes \
+            --enable-python3interp
+                else
+                    # ./configure --prefix="$HOME/.local" --with-features=huge --with-x  --enable-gui=auto --enable-xim --enable-fontset  --enable-python3interp --enable-multibyte --enable-gtk3-check
+                    ./configure \
+                        --prefix=$HOME/.local \
+                        --with-features=huge \
+                        --with-x  \
+                        --enable-gui=gtk3 \
+                        --enable-python3interp \
+                        --enable-gtk3-check
+    fi
 else
-    ./configure --prefix="$HOME/.local" --disable-gui --without-x  --enable-python3interp 
+    ./configure \
+        --prefix=$HOME/.local \
+        --disable-gui \
+        --without-x  \
+        --enable-python3interp 
 fi 
-pip install --user pynvim
+python3 -m pip install --upgrade pip
+python3 -m pip install --user pynvim
 make
 make install
 popd || exit 2
