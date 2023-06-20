577b3ba898ee737ccdfff3cd09ba1bd1f78dd047
Raveen Kumar <raveen.srk.kumar@gmail.com>
2023-06-20 00:49:18 -0500

    .

--- stow/.scripts/,dotscripts_checker.bash
+++ stow/.scripts/,dotscripts_checker.bash
@@ -2,17 +2,21 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-[ -d $HOME/.scripts ] || exit 0 # Exit script if the directory is not present
 
 
-readarray -t list_of_new_scripts < <(find $HOME/.scripts -type f)
+check_dir () {
+    local dir="$1"
+    [ -d $HOME/.$dir ] || exit 2 # Exit script if the directory is not present
+    local list_of_new_scripts=()
+    readarray -t list_of_new_scripts < <(find $HOME/.$dir -type f | grep -v "custom")
+    for f in "${list_of_new_scripts[@]}"; do
+        echo "$f"
+        name=$(basename "$f")
+        echo "where do you want to move this file?"
+        location=$(fzf --height=30% < <(find $HOME/my_repos | grep "\.$dir$"; echo "$HOME/.$dir"))
+        command mv -iv "$f" "$location"
+    done
+}
 
-new_files=()
-for f in "${list_of_new_scripts[@]}"; do
-    echo "$f"
-    name=$(basename "$f")
-    echo "where do you want to move this file?"
-    location=$(fzf --height=30% < <(find $HOME/my_repos | grep "\.scripts$"; echo "$HOME/.scripts"))
-    command mv -iv "$f" "$location"
-done
-
+check_dir "scripts"
+check_dir "my_bash_aliases"
