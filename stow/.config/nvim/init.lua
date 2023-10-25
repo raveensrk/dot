288c8edd633c6a0ada0249ce2a53cfe7d90cbd34
Raveen Kumar <raveen.srk.kumar@gmail.com>
2023-10-25 10:46:48 +0530

    Update macos packages list

--- stow/.config/nvim/init.lua
+++ stow/.config/nvim/init.lua
@@ -1,13 +1,14 @@
 vim.cmd([[
- source ~/.vimrc   
-]])
-    
-require('plugins')
+let mapleader=" "
+set winbar=%f
+set laststatus=3
+set cmdheight=0
+set mousescroll=ver:1,hor:1
+set rnu nu 
+" let &stc='%#NonText#%{&nu?v:lnum:""}%=%{&rnu&&(v:lnum%2)?"\ ".v:relnum:""}%#LineNr#%{&rnu&&!(v:lnum%2)?"\ ".v:relnum:""}'
+set jumpoptions=view
+nmap <leader>w :w<cr>
+nmap <leader>% :%so<cr>
 
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-  augroup end
 ]])
 
