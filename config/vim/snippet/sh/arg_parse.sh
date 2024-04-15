aa72e6d9fbbc11721c44f440744b3700ede18893
raveen <me@me.com>
2024-04-15 16:36:43 +0530

    Update

--- config/vim/snippet/sh/arg_parse.sh
+++ config/vim/snippet/sh/arg_parse.sh
@@ -1,25 +1,29 @@
 help() {
 	cat <<-HERE
-	Usage
-	-----------------
+		Usage
+		-----------------
 
 	HERE
+	exit 0
 }
 
 while [ "$#" -ne 0 ]; do
-      case "$1" in
-           --help|-h)
-                help
-                exit 0
-                ;;
-           --input|-i)
-                shift
-                input="$1"
-                ;;
-           --output|-o)
-                shift
-                output="$1"
-                ;;
-      esac
-      shift
+	case "$1" in
+	--help | -h)
+		help
+		;;
+	--input | -i)
+		shift
+		input="$1"
+		;;
+	--output | -o)
+		shift
+		output="$1"
+		;;
+	*)
+		echo "Unknown Argument: $1!"
+		exit 2
+		;;
+	esac
+	shift
 done
