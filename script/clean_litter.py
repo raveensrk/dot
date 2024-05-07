90bbce7265de20992981b64ed97706db6279c529
me <me@me.com>
2024-05-08 02:19:14 +0530

    Update fff, ffd

--- script/clean_litter.py
+++ script/clean_litter.py
@@ -8,8 +8,10 @@ import glob
 import itertools
 import shutil
 from rich.console import Console
+from rich import print
 
 console = Console()
+
 
 def main():
     """
@@ -39,21 +41,22 @@ def main():
 
     files = ["*#*", "*~*", "*.dcv", ".DS_Store", "*.SVM"]
 
-    found=[]
+    found = []
     with console.status("Deleting junk files...", spinner="dots12"):
         for path, pattern in list(itertools.product(paths, files)):
-            glob_path=os.path.expanduser(path)+"/**/"+pattern
+            glob_path = os.path.expanduser(path) + "/**/" + pattern
             # glob_path=os.path.expanduser(path)+"/"+pattern
             # print(glob_path)
-            found=glob.glob(os.path.expanduser(glob_path), recursive=True, include_hidden=True)
+            found = glob.glob(
+                os.path.expanduser(glob_path), recursive=True, include_hidden=True
+            )
             if len(found) != 0:
-                print(found)
+                print(f"{found = }")
                 for file in found:
                     if os.path.isdir(file):
                         shutil.rmtree(os.path.abspath(file))
                     else:
                         os.remove(file)
-
 
 
 def delete_empty_directories():
@@ -81,21 +84,23 @@ def delete_empty_directories():
         "~/work",
     ]
 
-    ignore = [
-            '.git'
-            ]
+    ignore = [".git"]
 
     for root in paths:
-        found=glob.glob(os.path.abspath(os.path.expanduser(root)+"/**/*"), recursive=True, include_hidden=True)
+        found = glob.glob(
+            os.path.abspath(os.path.expanduser(root) + "/**/*"),
+            recursive=True,
+            include_hidden=True,
+        )
         for path in found:
-            present=False
+            present = False
             for i in ignore:
                 if i in path:
-                    present=True
+                    present = True
             if not present:
                 if os.path.isdir(os.path.abspath(path)):
                     if not os.listdir(path):
-                        print(f'{path=}')
+                        print(f"Removing {path = }")
                         os.removedirs(path)
 
 
