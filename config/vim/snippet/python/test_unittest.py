01d5dfcfb3ceab6fdd68aac633557318fcbac4bd
me <me@me.com>
2024-05-18 21:10:50 +0530

    .

---
+++ config/vim/snippet/python/test_unittest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Test constant mode logic
+"""
+import unittest
+
+class TestClass(unittest.TestCase):
+    """
+    .
+    """
+
+    def test_one(self):
+        """
+        .
+        """
+
+
+
+if __name__ == "__main__":
+    unittest.main()
