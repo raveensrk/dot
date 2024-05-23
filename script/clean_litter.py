624425dea089a9b012295e05c525e2ddcb95c39f
me <me@me.com>
2024-05-23 21:38:35 +0530

    .

--- script/clean_litter.py
+++ script/clean_litter.py
@@ -7,82 +7,39 @@ import os
 import glob
 import itertools
 import shutil
-from rich.console import Console
-from rich import print
-
-console = Console()
 
 
-def main():
+def delete_junk_files(paths: list[str]):
     """
     Some main function
     """
 
-    paths = [
-        "~/Desktop",
-        "~/Documents",
-        "~/Downloads",
-        "~/Mail",
-        "~/bin",
-        "~/bookmarks.md",
-        "~/capture.md",
-        "~/done.txt",
-        "~/dot",
-        "~/fcu2",
-        "~/icloud",
-        "~/personal_repo",
-        "~/project",
-        "~/projects.txt",
-        "~/python_venv",
-        "~/script",
-        "~/tmp",
-        "~/work",
-    ]
-
     files = ["*#*", "*~*", "*.dcv", ".DS_Store", "*.SVM"]
 
     found = []
-    with console.status("Deleting junk files...", spinner="dots12"):
-        for path, pattern in list(itertools.product(paths, files)):
-            glob_path = os.path.expanduser(path) + "/**/" + pattern
-            # glob_path=os.path.expanduser(path)+"/"+pattern
-            # print(glob_path)
-            found = glob.glob(
-                os.path.expanduser(glob_path), recursive=True, include_hidden=True
-            )
-            if len(found) != 0:
-                print(f"{found = }")
-                for file in found:
-                    if os.path.isdir(file):
-                        shutil.rmtree(os.path.abspath(file))
-                    else:
-                        os.remove(file)
+    print("Deleting junk files...")
+    for path, pattern in list(itertools.product(paths, files)):
+        glob_path = os.path.expanduser(path) + "/**/" + pattern
+        # glob_path=os.path.expanduser(path)+"/"+pattern
+        # print(glob_path)
+        found = glob.glob(
+            os.path.expanduser(glob_path), recursive=True, include_hidden=True
+        )
+        if len(found) != 0:
+            print(f"{found = }")
+            for file in found:
+                if os.path.isdir(file):
+                    shutil.rmtree(os.path.abspath(file))
+                else:
+                    os.remove(file)
 
 
-def delete_empty_directories():
+def delete_empty_directories(paths: list):
     """
-    This function will find and delete_empty_directories in a given path recursive
+    This function will find and delete_empty_directories
+    in a given path recursive
     """
-    paths = [
-        "~/Desktop",
-        "~/Documents",
-        "~/Downloads",
-        "~/Mail",
-        "~/bin",
-        "~/bookmarks.md",
-        "~/capture.md",
-        "~/done.txt",
-        "~/dot",
-        "~/fcu2",
-        "~/icloud",
-        "~/personal_repo",
-        "~/project",
-        "~/projects.txt",
-        "~/python_venv",
-        "~/script",
-        "~/tmp",
-        "~/work",
-    ]
+    print("Deleting empty directories...")
 
     ignore = [".git"]
 
@@ -105,7 +62,22 @@ def delete_empty_directories():
 
 
 if __name__ == "__main__":
-    main()
-    with console.status("Deleting empty directories...", spinner="dots12"):
-        delete_empty_directories()
+    paths1 = [
+        "~/Desktop",
+        "~/Documents",
+        "~/Downloads",
+        "~/Mail",
+        "~/bin",
+        "~/dot",
+        "~/fcu2",
+        "~/icloud",
+        "~/personal_repo",
+        "~/project",
+        "~/python_venv",
+        "~/script",
+        "~/tmp",
+        "~/work",
+    ]
+    delete_junk_files(paths1)
+    delete_empty_directories(paths1)
 # find_broken_symlinks "$HOME" TODO
