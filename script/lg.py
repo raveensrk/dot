5ca126611f96596f2450deb7eb4cdd20e1bf3769
Raveen Kumar <raveen.srk.kumar@gmail.com>
2024-07-26 12:32:58 +0530

    Added script to smoother git commit

---
+++ script/lg.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""
+Read a file containing list of git repositories and run lazygit in that
+"""
+
+import subprocess
+import os
+
+list_of_repositories = os.path.expanduser("~/script/list_of_repositories.txt")
+
+with open(list_of_repositories, "r", encoding="UTF-8") as file_pointer:
+    repos = file_pointer.readlines()
+
+print(repos)
+
+for repo in repos:
+    repo = os.path.expanduser(repo.rstrip())
+    os.chdir(repo)
+    subprocess.run(["git", "fetch"], check=True)
+    subprocess.run(["lazygit"], check=True)
