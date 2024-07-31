dd1922a5084e06f5481e53838283fe566a3e6a3d
Raveen Kumar <raveen.srk.kumar@gmail.com>
2024-07-31 19:15:36 +0530

    Added a template for chatgpt python

---
+++ config/vim/skeleton/python/chatgpt_prompt.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module will convert the given plain text file to json file. Each line in the plain text file will be a json entry. The format of json file is an array of objects
+"""
+
+def main(in_file, out_file):
+    """
+    Read in_file as plain text
+    Write to out_file as json
+    """
+
+if __name__ == "__main__":
+    main()
