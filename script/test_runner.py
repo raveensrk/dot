0410065101c3e48e6a8ac7f33efd775211ee119e
Raveen Kumar <raveen.srk.kumar@gmail.com>
2024-08-02 10:39:46 -0500

    .

---
+++ script/test_runner.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+A script to run tests. Read a file from --tests or -t and execute each line one
+by one. Run asynchronously when possible. Test file must be a json file.
+"""
+
+import json
+import subprocess
+import argparse
+
+parser = argparse.ArgumentParser("Simple test runner")
+parser.add_argument("--tests", type=str, required=True, help="tests.json file")
+args = parser.parse_args()
+
+with open(args.tests, "r", encoding="UTF-8") as file_pointer:
+    tests = json.load(file_pointer)
+
+for obj in tests:
+    subprocess.run(obj["cmd"].split(), check=True)
