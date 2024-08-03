c54d002e849a8f9f1a4d6de72a600c0e9b2eca82
Raveen Kumar <raveen.srk.kumar@gmail.com>
2024-08-03 21:41:52 +0530

    Update new script lg2.py

---
+++ script/lg2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+"""
+Read a file containing list of git repositories and run lazygit in that
+"""
+
+import subprocess
+import os
+
+list_of_repositories = os.path.expanduser("~/script/list_of_repositories.txt")
+
+with open(list_of_repositories, "r", encoding="UTF-8") as file_pointer:
+    repos = file_pointer.readlines()
+
+
+for repo in repos:
+    repo = os.path.expanduser(repo.rstrip())
+    print(f"Repo: {repo}")
+    os.chdir(repo)
+    result = subprocess.run(
+        ["git", "status", "--porcelain"],
+        check=True,
+        capture_output=True,
+        text=True,
+    )
+    # print(type(result))
+    # stdout = result.stdout
+    lines = result.stdout.split("\n")
+    lines = [line for line in lines if line.strip()]
+    print("Number of lines: %0d" % len(lines))
+    if len(lines) != 0:
+        subprocess.run(["lazygit"], check=True)
+
+    subprocess.run(["git", "pull"], check=True)
+    subprocess.run(["git", "push"], check=True)
