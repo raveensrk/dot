480649da89c000730ae475a8b9d5d67227e49e70
Raveen Kumar <raveen.srk.kumar@gmail.com>
2024-08-03 17:00:45 +0530

    .

--- script/lg.py
+++ script/lg.py
@@ -13,7 +13,7 @@ with open(list_of_repositories, "r", encoding="UTF-8") as file_pointer:
 
 for repo in repos:
     repo = os.path.expanduser(repo.rstrip())
-    print("Repo: %s", repo)
+    print("Repo: %s" % repo)
     os.chdir(repo)
     # TODO:
     # If git fetch is clean and If git pull is clean
