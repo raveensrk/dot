c5a051a766e092c0af59723109b2349dfee65aaf
Raveen Kumar <raveen.srk.kumar@gmail.com>
2024-08-16 18:23:17 +0530

    Create dry_run_option.py

---
+++ config/vim/snippet/python/dry_run_option.py
@@ -0,0 +1,7 @@
+    parser.add_argument(
+        "-n",
+        "--dry-run",
+        required=False,
+        action="store_true",
+        help="Dry run. Do not execute the commands, only print what is going to happen.",
+    )
