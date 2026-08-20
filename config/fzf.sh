d0190893da0896ff4fddf5e1b0f4bed78fcefe6b
Raveen Kumar <raveen.srk.kumar@gmail.com>
2026-08-21 02:57:54 +0530

    added vv alias for opening vim with fzf files are input argument

--- config/fzf.sh
+++ config/fzf.sh
@@ -4,3 +4,4 @@ export FZF_DEFAULT_OPTS="--keep-right --layout=reverse +s -e --history=$HOME/.fz
 export FZF_COMPLETION_OPTS="--keep-right --layout=reverse +s -e --history=$HOME/.fzf_history"
 export RIPGREP_CONFIG_PATH="$DOT/config/ripgrep"
 eval "$(fzf --bash)"
+alias vv="vim \$(fzf)"
